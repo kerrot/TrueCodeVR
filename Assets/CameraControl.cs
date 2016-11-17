a75695b6930c2302d6cafea1e47e08ed3264535f
kerrot <kerrot.gs@gmail.com>
2016-11-17 17:18:04 +0900

    ブラウン管　エフェクト

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        SceneControler sc = GameObject.FindObjectOfType<SceneControler>();
+        if (sc && sc.LoadFromAnother)
+        {
+            Fadout();
+        }
+    }
+
+    public void FadinComplete()
+    {
+        SceneControler sc = GameObject.FindObjectOfType<SceneControler>();
+        if (sc)
+        {
+            sc.DoChangeScene();
+        }
+    }
+
+    public void Fadin()
+    {
+        anim.SetTrigger("Fadin");
+    }
+
+    public void Fadout()
+    {
+        anim.SetTrigger("Fadout");
+    }
+}
---
+++ Assets/SceneControler.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneControler : MonoBehaviour {
+    public bool LoadFromAnother { get { return loadFromAnother; } }
+
+    AsyncOperation async;
+
+    bool loadFromAnother;
+    bool loading;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void LoadScene(string name)
+    {
+        CameraControl crt = Camera.main.gameObject.GetComponent<CameraControl>();
+        if (crt)
+        {
+            crt.Fadin();
+        }
+
+        StartCoroutine(LoadingAsync(name));
+    }
+
+    private IEnumerator LoadingAsync(string sceneName)
+    {
+        async = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        loading = true;
+        async.allowSceneActivation = false;
+        while (async.progress < 0.9f)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        loading = false;
+    }
+
+    public void DoChangeScene()
+    {
+        StartCoroutine(ChangeScene());
+    }
+
+    private IEnumerator ChangeScene()
+    {
+        while (loading)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        async.allowSceneActivation = true;
+        loadFromAnother = true;
+    }
+}
