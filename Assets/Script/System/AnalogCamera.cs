a721267c5fcea78a0d5a811b71788763043f7797
kerrot <kerrot.gs@gmail.com>
2017-03-18 16:55:41 +0900

    analog camera test

---
+++ Assets/Script/System/AnalogCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AnalogCamera : MonoBehaviour {
+
+    [SerializeField]
+    private List<Camera> cameras = new List<Camera>();
+
+    private void Start()
+    {
+        Activate(0);
+    }
+
+    public void Activate(int num)
+    {
+        if (num >= 0 && num < cameras.Count)
+        {
+            cameras.ForEach(c => c.gameObject.SetActive(false));
+            cameras[num].gameObject.SetActive(true);
+        }
+    }
+}
