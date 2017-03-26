265bd8c06bd344e7c121665e23cb2e038a598f78
kerrot <kerrot.gs@gmail.com>
2017-03-26 17:01:53 +0900

    weightscale test

---
+++ Assets/Script/ActObject/WeightScale.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class WeightScale : MonoBehaviour {
+    [SerializeField]
+    private GameObject needle;
+    [SerializeField]
+    private GameObject stand;
+    [SerializeField]
+    private float maxWeight;
+
+
+
+    private void Awake()
+    {
+        
+    }
+
+    
+}
